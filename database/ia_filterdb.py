0ef635c7f003345586cb9f015c240559229b3407
Henock Joy <89825644+henockjoy@users.noreply.github.com>
2026-05-24 20:43:20 +0400

    Modify indexes in ia_filterdb.py
    Updated indexes in Meta class to include composite index on file_name and caption.

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -47,8 +47,13 @@ class Media(Document):
     caption = fields.StrField(allow_none=True)
 
     class Meta:
-        indexes = ('$file_name', )
         collection_name = COLLECTION_NAME
+        indexes = [
+            {
+                "key": [("file_name", "text"), ("caption", "text")],
+                "name": "file_name_caption_text"
+            }
+        ]
 
 async def choose_mediaDB():
     """Always use Media as the database."""
